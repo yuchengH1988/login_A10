da21e9fb5538996f7ff5600bdeeb1fcd98f9729b
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-05-18 13:34:55 +0800

    install express-handlebars , build files.hbs

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const port = 3000
 const app = express()
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('login')
 })
 
 // 設定 port 3000
 app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+  console.log(`App is running on http://localhost:${port}`)
 })
